106601f8739edffc90e61a45380610945b767ccd
sciphymer <sciphymer@gmail.com>
2023-04-18 22:43:36 +0800

    Add 2 test cases

---
+++ src/test/java/CurrencyConverterTest.java
@@ -0,0 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import project.CurrencyConverter.CurrencyConverter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+
+public class CurrencyConverterTest {
+
+    private final PrintStream standardOut = System.out;
+
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
+    @Test
+    public void missingArgument(){
+        String[] inputArgument = new String[]{"-a","10000","-f","USD","-t","HKD"};
+        CurrencyConverter.main(inputArgument);
+        Assertions.assertTrue(outputStreamCaptor.toString().trim().contains(
+                "Missing required options: i, f, t, a\n" +
+                        "usage: currency-converter\n" +
+                        "-a,--amount <arg>      amount\n" +
+                        "-f,--from <arg>        from currency\n" +
+                        "-i,--input <arg>       input file path\n" +
+                        "-t,--to <arg>          too currency"
+        ));
+    }
+
+    @Test
+    public void correctArgument(){
+        String fxRefFilePath = "src/main/resources/eurofxref-daily.xml";
+        String[] inputArgument = new String[]{"-a","10000","-f","USD","-t","HKD","-i",fxRefFilePath};
+        CurrencyConverter.main(inputArgument);
+        Assertions.assertTrue(outputStreamCaptor.toString().trim().contains(
+                "From: USD | To: HKD | Amount: 10000 | Result: HKD 78499.22"
+        ));
+    }
+}
